1483e8ecc836a71e7b02f9a86e7c05ef7ce0e749
Github User Name <you@example.com>
2018-12-23 15:07:33 +0300

    created the html files with code at this commit .

---
+++ products/views.py
@@ -0,0 +1,45 @@
+from django.http  import HttpResponse,Http404
+from django.shortcuts import render
+import datetime as dt
+
+
+# Create your views here.
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def stalk_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def past_days_stalk(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
